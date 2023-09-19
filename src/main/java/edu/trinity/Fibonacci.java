1c573e4bc4224467caaf569941da443d46e6041a
armennanayan <127252750+armennanayan@users.noreply.github.com>
2023-09-19 19:43:22 -0400

    Completed Fibonacci class
    All tests passed

--- src/main/java/edu/trinity/Fibonacci.java
+++ src/main/java/edu/trinity/Fibonacci.java
@@ -3,6 +3,23 @@ package edu.trinity;
 public class Fibonacci {
 
     public static int calculate(int i) {
-        return 0;
+        int initialVar = 1;
+        int intermVar = 0;
+        int finalVal = 1;
+        if(i == 0){
+            return 0;
+        }
+        else if(i == 1 || i == 2){
+            return 1;
+        }
+        else{
+            for(int f = 3; f <= i; f++){
+                intermVar = finalVal;
+                finalVal = finalVal + initialVar;
+                initialVar = intermVar;
+            }
+        }
+       
+       return finalVal;
     }
 }
