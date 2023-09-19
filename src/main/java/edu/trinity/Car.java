6875a922c2a7beaf16f415b68e865a9acf6edb38
armennanayan <127252750+armennanayan@users.noreply.github.com>
2023-09-19 18:42:24 -0400

    Completed the implementation of the Car class
    Fixed all the issues there

--- src/main/java/edu/trinity/Car.java
+++ src/main/java/edu/trinity/Car.java
@@ -4,38 +4,64 @@ import java.time.Year;
 
 public class Car {
 
+    private String make;
+    private String model;
+    private Year year;
+    int miles = 0;
+    boolean running = false;
+
     public Car(String make, String model, Year year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
 
     }
 
     public String getMake() {
-        return null;
+        return make;
     }
 
     public String getModel() {
-        return null;
+        return model;
     }
 
     public Year getYear() {
-        return null;
+        return year;
     }
 
     public boolean isRunning() {
-        return false;
+        return running;
     }
 
     public void start() {
-
+        if(isRunning() == false){
+            running = true;
+        }
+        else{
+            return;
+        }
     }
 
     public void stop() {
+        if(isRunning()==true){
+            running = false;
+        }
+        else{
+            return;
+        }
 
     }
 
     public void drive(int distance) {
+        if(isRunning() == true){
+            miles += distance;
+        }
+        else{
+            return;
+        }    
     }
 
     public int getMiles() {
-        return 0;
+        return miles;
     }
 }
