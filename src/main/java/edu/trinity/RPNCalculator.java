c5e998e22b32cde6cade551917850675eb28040e
armennanayan <127252750+armennanayan@users.noreply.github.com>
2023-09-20 11:30:49 -0400

    Completed RPNCalculator
    Completed RPNCalculator and all the tests passed.

--- src/main/java/edu/trinity/RPNCalculator.java
+++ src/main/java/edu/trinity/RPNCalculator.java
@@ -1,7 +1,69 @@
 package edu.trinity;
 
+import java.util.EmptyStackException;
+import java.util.Stack;
+
 public class RPNCalculator {
+
     public static double evaluate(String expr) {
-        return 0.0;
+        String[] intStr = expr.split(" ");
+        Stack<Double> numStack = new Stack<>();
+        double num1 = 0;
+        double num2 = 0;
+        double finVal = 0;
+
+        for(String member:intStr){
+            if(checkOperator(member) == false){
+                numStack.push(Double.parseDouble(member));
+            }
+            else{
+                if(numStack.size()< 2){
+                    throw new EmptyStackException();
+                }
+                num2 = numStack.pop();
+                num1 = numStack.pop();
+                finVal = analyzeOperation(member, num1, num2);
+                numStack.push(finVal);
+            }
+        }
+
+        if(numStack.size() != 1){
+            numStack.pop();
+        }
+
+        return numStack.pop();
+
     }
+
+
+    public static boolean checkOperator(String operat){
+        switch(operat){
+            case "+": case "-": case "*": case "/":
+                return true;
+            default:
+                return false;
+        }
+
+    }
+
+
+    public static double analyzeOperation(String operation, double num1, double num2){
+        switch(operation){
+            case "+":
+                return num1 + num2;
+            case "-":
+                return num1 - num2;
+            case "*":
+                return num1 * num2;
+            case "/":
+                if(num2 == 0){
+                    throw new IllegalArgumentException();
+                }
+                return num1/num2;
+            default:
+                throw new IllegalArgumentException();
+    }
+
+        }
+        
 }
