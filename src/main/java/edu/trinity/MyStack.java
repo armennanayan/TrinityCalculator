018aa0296b4018b2a32f6ee9a8e578b75165d2e4
armennanayan <127252750+armennanayan@users.noreply.github.com>
2023-09-19 20:45:41 -0400

    Completed the MyStack Class
    Fixed all the issues and all the tests passed

--- src/main/java/edu/trinity/MyStack.java
+++ src/main/java/edu/trinity/MyStack.java
@@ -1,18 +1,54 @@
 package edu.trinity;
 
+import java.util.EmptyStackException;
+
 public class MyStack<T> {
+
+    Object[] arr = new Object[100]; //Assuming there is a limit of 100 n
+    int posit = 0;
+    T lastObj = null;
+    boolean status = false;
+
     public void push(T hello) {
+        arr[posit] = hello;
+        posit++;
     }
 
     public T peek() {
-        return null;
+        if(isEmpty() == true){
+            throw new EmptyStackException();
+        }
+        else{
+            lastObj = (T) arr[posit-1];
+            return lastObj;
+        }
     }
 
-    public T pop() {
-        return null;
+    public T pop(){
+        if(isEmpty()){
+            throw new EmptyStackException();
+        }
+        else{
+            if(posit > 0){
+                lastObj = (T) arr[posit-1];
+                arr[posit-1] = null;
+                posit--;
+            }
+            else{
+                lastObj = null;
+            }
+            return lastObj;
+        }
     }
 
     public boolean isEmpty() {
-        return false;
+        if(posit <= 0){
+            status = true;
+        }
+        else{
+            status = false;
+        }
+        return status;
     }
+
 }
